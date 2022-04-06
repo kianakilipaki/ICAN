1c7f73d7a6b56238be1f4fa36aa3fe3114df61fc
Alani Gilbert <44738610+kianakilipaki@users.noreply.github.com>
2022-04-06 14:31:56 -0600

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { environment } from 'src/environments/environment';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-import { FirebaseOptionsToken } from 'angularfire2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +22,17 @@ import { NewFormComponent } from './main/pages/reports/new-form/new-form.compone
 import { OpenFormComponent } from './main/pages/forms/open-form/open-form.component';
 import { OpenReportComponent } from './main/pages/reports/open-report/open-report.component';
 import { RoomListComponent } from './main/pages/messages/room-list.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -55,13 +60,19 @@ import { RoomListComponent } from './main/pages/messages/room-list.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    AngularFireModule,
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AngularFireStorageModule
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
   providers: [
-    { provide: FirebaseOptionsToken, useValue: environment.firebase }],
+    ScreenTrackingService,UserTrackingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBPzKMIkj4OHJFA6jgu8zRXuxtwlkUmF8g",
-    authDomain: "ican-2b124.firebaseapp.com",
-    projectId: "ican-2b124",
-    storageBucket: "ican-2b124.appspot.com",
-    messagingSenderId: "976008425635",
-    appId: "1:976008425635:web:683a1b6dbcab1d6e1a4f16",
-    measurementId: "G-QT2PNH4S4K"
+    projectId: 'ican-2b124',
+    appId: '1:976008425635:web:683a1b6dbcab1d6e1a4f16',
+    databaseURL: 'https://ican-2b124-default-rtdb.firebaseio.com',
+    storageBucket: 'ican-2b124.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBPzKMIkj4OHJFA6jgu8zRXuxtwlkUmF8g',
+    authDomain: 'ican-2b124.firebaseapp.com',
+    messagingSenderId: '976008425635',
+    measurementId: 'G-QT2PNH4S4K',
   }
 };
 
