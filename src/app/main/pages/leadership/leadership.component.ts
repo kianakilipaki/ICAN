ff50867a0b8d27dbd37a1b3ffbb83429b4c5beda
Alani Gilbert <44738610+kianakilipaki@users.noreply.github.com>
2022-08-29 17:03:53 -0600

    fix images

--- src/app/main/pages/leadership/leadership.component.ts
+++ src/app/main/pages/leadership/leadership.component.ts
@@ -8,8 +8,8 @@ import { Router } from '@angular/router';
 })
 export class LeadershipComponent {
 
-  profile = { id: 70, isPrimary: true, name: "Alan Smith", relation:"primary", pic: "/assets/ect/pic.jpg", title: "Bishop", email: "alansmith@gmail.com", phone: "(203)123-1234", address: "something st Orem, UT 22883" };
-  profile2 = {id: 71, isPrimary: false, name: "Sarah Smith", relation:"Spouse", pic: "/assets/ect/profile.svg", title: "Member", email:"sarahsmith@gmail.com", phone:"(203)123-1234", address:"something st Orem, UT 22883" };
+  profile = { id: 70, isPrimary: true, name: "Alan Smith", relation:"primary", pic: "/assets/etc/pic.jpg", title: "Bishop", email: "alansmith@gmail.com", phone: "(203)123-1234", address: "something st Orem, UT 22883" };
+  profile2 = {id: 71, isPrimary: false, name: "Sarah Smith", relation:"Spouse", pic: "/assets/etc/profile.svg", title: "Member", email:"sarahsmith@gmail.com", phone:"(203)123-1234", address:"something st Orem, UT 22883" };
   profiles = [this.profile, this.profile2];
   group1 = { name: "Ward Council", members: this.profiles };
   groups = [this.group1, this.group1];
--- src/app/main/pages/profile/profile.component.ts
+++ src/app/main/pages/profile/profile.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent {
   open = false;
-  profile = { isPrimary: true, name: "Alan Smith", relation:"primary", pic: "/assets/ect/pic.jpg", title: "Bishop", email: "alansmith@gmail.com", phone: "(203)123-1234", address: "something st Orem, UT 22883" };
-  profile2 = { isPrimary: false, name: "Sarah Smith", relation:"Spouse", pic: "/assets/ect/profile.svg", title: "Member", email:"sarahsmith@gmail.com", phone:"(203)123-1234", address:"something st Orem, UT 22883" };
+  profile = { isPrimary: true, name: "Alan Smith", relation:"primary", pic: "/assets/etc/pic.jpg", title: "Bishop", email: "alansmith@gmail.com", phone: "(203)123-1234", address: "something st Orem, UT 22883" };
+  profile2 = { isPrimary: false, name: "Sarah Smith", relation:"Spouse", pic: "/assets/etc/profile.svg", title: "Member", email:"sarahsmith@gmail.com", phone:"(203)123-1234", address:"something st Orem, UT 22883" };
   profiles = [this.profile, this.profile2];
 
   addProfile() {
